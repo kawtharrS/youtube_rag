5acf5f7aeb9e377977f5f531afce98503511b30f
Kawthar Shaalan <101220311@mu.edu.lb>
2026-06-13 16:58:14 +0300

    fixed the chat model class

--- chat_model.py
+++ chat_model.py
@@ -7,6 +7,3 @@ load_dotenv()
 OPENAI_API_KEY= os.environ["OPENAI_API_KEY"]
 
 model = ChatOpenAI(model="gpt-5.5", api_key=OPENAI_API_KEY)
-
-response = model.invoke("Hello!")
-print(response.content)
