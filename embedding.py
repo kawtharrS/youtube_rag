b4c91e19f209212f40ff7c517119454b2182ab10
Kawthar Shaalan <101220311@mu.edu.lb>
2026-06-13 18:01:10 +0300

    added embeddings into vector store

--- embedding.py
+++ embedding.py
@@ -2,8 +2,10 @@ import getpass
 import os
 from dotenv import load_dotenv 
 from langchain_openai import OpenAIEmbeddings
+from langchain_core.vectorstores import InMemoryVectorStore
 
 load_dotenv()
 OPENAI_API_KEY= os.environ["OPENAI_API_KEY"]
 
 embeddings = OpenAIEmbeddings(model="text-embedding-3-large", api_key =OPENAI_API_KEY )
+vector_store = InMemoryVectorStore(embeddings)
